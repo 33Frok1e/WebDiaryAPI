af9cc001f0a60ce322d51e042411a67f9de6b928
33Frok1e <mratikrishna@gmail.com>
2024-11-13 23:47:32 +0530

    Add project files.

---
+++ WebDiaryApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebDiaryApi.Model;
+
+namespace WebDiaryApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
